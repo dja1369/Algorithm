b274f1a3b80746c2c8be22f0579c66e599f48f6f
LDS <vpfmzmtm@deepnoid.com>
2024-06-18 09:27:46 +0900

    재활훈련

--- backjoon/b_1018.py
+++ backjoon/b_1018.py
@@ -10,12 +10,12 @@ for i in range(n-7):
             for b in range(j, j+8):
                 # 시작점의 색은 검정, 하양 이다
                 if(a+b)%2==0: # 짝수 일 경우
-                    if board[a][b] == 'B':  # 짝수의 경우 일정한 색을 가지게 됨
+                    if board[a][b] == 'B':  # 인덱스의 합을 보고 홀수의 경우와 짝수의 경우의 색상이 나옴
                         is_white+=1
                     else:
                         is_black+=1
                 else:   # 홀수 일 경우
-                    if board[a][b] == 'B':  # 홀수의 경우 일정한 색을 가지게 됨
+                    if board[a][b] == 'B':
                         is_black+=1
                     else:
                         is_white+=1
