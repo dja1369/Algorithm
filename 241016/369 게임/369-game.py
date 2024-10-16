e861a96536cbbb991ca7524f5a8a209dfd3bfa6f
Deok su Lee <qjxlftn@gmail.com>
2024-10-16 23:30:24 +0900

    Title: 369 게임, Time: 101ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 241016/369 게임/369-game.py
+++ 241016/369 게임/369-game.py
@@ -1,7 +1,13 @@
 n = int(input())
 
 for i in range(1, n+1):
-    if i % 3 == 0 or str(i) in [3,6,9]:
+    if i % 3 == 0:
         print(0, end=" ")
+        continue
+    for j in str(i):
+        if j in [3,6,9]:
+            print(0, end=" ")
+            break
     else:
         print(i, end=" ")
+        continue
