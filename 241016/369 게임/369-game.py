4a9a946ee85be8a445dffec7e8f21642b0513211
Deok su Lee <qjxlftn@gmail.com>
2024-10-16 23:32:43 +0900

    Title: 369 게임, Time: 106ms, Memory: 24MB, Status: Passed - CodeTree

--- 241016/369 게임/369-game.py
+++ 241016/369 게임/369-game.py
@@ -5,7 +5,7 @@ for i in range(1, n+1):
         print(0, end=" ")
         continue
     for j in str(i):
-        if j in [3,6,9]:
+        if j in ["3","6","9"]:
             print(0, end=" ")
             break
     else:
