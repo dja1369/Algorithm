c3a46b233d7a531950961b7db7536a0853f0aeb0
Deok su Lee <qjxlftn@gmail.com>
2024-10-16 23:19:20 +0900

    Title: a 부터 b 까지, Time: 108ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 241016/a 부터 b 까지/a-to-b.py
+++ 241016/a 부터 b 까지/a-to-b.py
@@ -1,12 +1,13 @@
 a, b = map(int, input().split())
 
 result = a
-for _ in range(a, b):
-    if result > b:
-        break 
+for _ in range(a, b+1):
+    if result >= b:
+        break
+    print(result, end=" ")
     if result % 2 != 0:
-        print(result, end=" ")
         result *= 2  
+        print(result, end=" ")  
     else:
-        print(result, end=" ")
         result += 3
+        print(result, end=" ")
